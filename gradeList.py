549c366317a1b2d2f5cb80b4f9ccb817fe2bbd75
Miranda <mirandahong31@gmail.com>
2025-03-08 14:00:32 -0500

    Update gradeList.py

--- gradeList.py
+++ gradeList.py
@@ -1,15 +1,28 @@
-def add_assessment():
+#add a course
+def add_course():
     course_list = []
+    while True:
+        course = input("Enter course name: ")
+        course_list.append(course)
+        user = input("Add another course? (y/n): ")
+        if user == 'n':
+            break
+    return course_list
+
+#add an assessment
+def add_assessment():
+    grade_list = []
     while True:
         print('Enter assessment details: ')
         name = input('Assessment title: ')
         grade = int(input('Grade: '))
         weight = float(input('Weight: '))
         assessment = [name,grade,weight]
-        course_list.append(assessment)
+        grade_list.append(assessment)
         user = input('Add another assessment? (y/n): ')
         if user == 'n':
             break
-    return course_list
-    
-print(add_assessment())
+    return grade_list
+
+print('Course List: ',add_course())
+print('Assessment List: ',add_assessment())
