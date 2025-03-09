e43602ea630c34a846c0d4222210ae532ad0eb70
AkodingZ <ak.codingstudent@gmail.com>
2025-03-08 22:20:41 -0500

    Has basic UI
    make sure when website is up your switch to the base page with  /base in the url

--- basic-website/backend/app.py
+++ basic-website/backend/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend/templates')
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
 @app.route('/')
 def home():
     return render_template('index.html')
